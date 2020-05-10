c58830149da6af96c1375ed5eaaaaf9108332138
siyual.bak <kdpark0723@gmail.com>
2020-05-10 11:03:32 +0900

    init

---
+++ src/main/kotlin/com/ara/meno/MenoApplication.kt
@@ -0,0 +1,11 @@
+package com.ara.meno
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MenoApplication
+
+fun main(args: Array<String>) {
+	runApplication<MenoApplication>(*args)
+}
