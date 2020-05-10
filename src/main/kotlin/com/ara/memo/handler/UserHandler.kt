cb5c4655f98706dd8fd3445d4efd690ad5667c3a
siyual.bak <kdpark0723@gmail.com>
2020-05-10 19:44:29 +0900

    refactoring

--- src/main/kotlin/com/ara/memo/handler/UserHandler.kt
+++ src/main/kotlin/com/ara/memo/handler/UserHandler.kt
@@ -3,9 +3,7 @@ package com.ara.memo.handler
 import com.ara.memo.service.user.UserService
 import com.ara.memo.view.user.UserView
 import org.springframework.http.MediaType
-import org.springframework.http.codec.json.Jackson2CodecSupport
 import org.springframework.stereotype.Component
-import org.springframework.web.reactive.function.BodyInserters
 import org.springframework.web.reactive.function.server.ServerRequest
 import org.springframework.web.reactive.function.server.ServerResponse
 import reactor.core.publisher.Mono
@@ -13,22 +11,22 @@ import reactor.core.publisher.Mono
 @Component
 class UserHandler(
     private val service: UserService
-) {
+) : Handler() {
     fun create(request: ServerRequest): Mono<ServerResponse> {
         val uri = request.uri()
-        val userView = request.bodyToMono(UserView::class.java)
+        val requestBody = request.bodyToMonoWithView(
+            UserView::class,
+            UserView.CreateRequestView::class
+        )
+        val user = requestBody.map { it.toUser() }
+            .flatMap(service::create)
 
         return ServerResponse.created(uri)
             .contentType(MediaType.APPLICATION_JSON)
-            .hint(Jackson2CodecSupport.JSON_VIEW_HINT, UserView.CreateResponseView::class.java)
-            .body(
-                BodyInserters.fromPublisher(
-                    userView
-                        .map { it.toUser() }
-                        .flatMap(service::create)
-                        .map { UserView.from(it) },
-                    UserView::class.java
-                )
+            .bodyWithView(
+                user.map{ UserView.from(it) },
+                UserView::class,
+                UserView.CreateResponseView::class
             )
     }
 }
