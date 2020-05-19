163bf975df5aed0e4346944e92ca30a19dc91391
siyual.park <kdpark0723@gmail.com>
2020-05-19 21:25:10 +0900

    Refactoring

--- src/main/kotlin/com/ara/memo/config/SwaggerConfig.kt
+++ src/main/kotlin/com/ara/memo/config/SwaggerConfig.kt
@@ -6,7 +6,6 @@ import org.springframework.context.annotation.Configuration
 import reactor.core.publisher.Flux
 import reactor.core.publisher.Mono
 import springfox.documentation.builders.RequestHandlerSelectors
-import springfox.documentation.schema.AlternateTypeRule
 import springfox.documentation.spi.DocumentationType
 import springfox.documentation.spring.web.plugins.Docket
 import springfox.documentation.swagger2.annotations.EnableSwagger2WebFlux
