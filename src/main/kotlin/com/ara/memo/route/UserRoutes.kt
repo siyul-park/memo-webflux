693670b43d45e7caf8bca573edc4ac5762cb005e
siyual.park <kdpark0723@gmail.com>
2020-06-20 19:55:45 +0900

    필요하지 않는것 제거

--- src/main/kotlin/com/ara/memo/route/UserRoutes.kt
+++ src/main/kotlin/com/ara/memo/route/UserRoutes.kt
@@ -1,18 +1,13 @@
 package com.ara.memo.route
 
 import com.ara.memo.handler.UserHandler
-import org.slf4j.LoggerFactory
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.http.MediaType.APPLICATION_JSON
 import org.springframework.web.reactive.function.server.router
 
 @Configuration
-class UserRoutes(
-    private val handler: UserHandler
-) {
-    private val logger = LoggerFactory.getLogger(UserRoutes::class.java)
-
+class UserRoutes(private val handler: UserHandler) {
     @Bean
     fun userRouter() = router {
         (accept(APPLICATION_JSON) and "/users").nest {
