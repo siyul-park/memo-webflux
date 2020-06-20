2b22a8ca266d854967590f311cb788932184375a
siyual.park <kdpark0723@gmail.com>
2020-06-20 21:45:10 +0900

    리팩토링

--- src/main/kotlin/com/ara/memo/dao/user/plugin/UserDaoPluginComponent.kt
+++ src/main/kotlin/com/ara/memo/dao/user/plugin/UserDaoPluginComponent.kt
@@ -1,27 +1,16 @@
 package com.ara.memo.dao.user.plugin
 
 import com.ara.memo.dao.plugin.SpringExceptionMappingPlugin
+import com.ara.memo.util.plugin.Plugin
 import com.ara.memo.util.plugin.PluginManager
 import com.ara.memo.util.plugin.ScheduleSubscribePlugin
 import org.springframework.stereotype.Component
-import reactor.core.publisher.Flux
-import reactor.core.publisher.Mono
 import reactor.core.scheduler.Scheduler
 
 @Component
 class UserDaoPluginComponent(
     scheduler: Scheduler
-) : UserDaoPlugin {
-    private val pluginManager = PluginManager()
-
-    init {
-        pluginManager
-            .addPlugin(ScheduleSubscribePlugin(scheduler))
-            .addPlugin(SpringExceptionMappingPlugin)
-            .addPlugin(UserExceptionMappingPlugin)
-    }
-
-    override fun <T> apply(mono: Mono<T>) = pluginManager.apply(mono)
-
-    override fun <T> apply(flux: Flux<T>) = pluginManager.apply(flux)
-}
+) : UserDaoPlugin, Plugin by PluginManager()
+    .addPlugin(ScheduleSubscribePlugin(scheduler))
+    .addPlugin(SpringExceptionMappingPlugin)
+    .addPlugin(UserExceptionMappingPlugin)
