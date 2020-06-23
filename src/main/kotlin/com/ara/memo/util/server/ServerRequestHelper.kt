6cbcab7ac1d69a134fa200edfad8ce607ed8be71
siyual.park <kdpark0723@gmail.com>
2020-06-23 22:58:40 +0900

    Refactoring

--- src/main/kotlin/com/ara/memo/util/server/ServerRequestHelper.kt
+++ src/main/kotlin/com/ara/memo/util/server/ServerRequestHelper.kt
@@ -1,17 +1,11 @@
 package com.ara.memo.util.server
 
 import org.springframework.web.reactive.function.server.ServerRequest
-import reactor.core.publisher.Flux
-import reactor.core.publisher.Mono
 import java.util.concurrent.ConcurrentHashMap
-import kotlin.reflect.KClass
 
 fun ServerRequest.hint(key: String, value: Any) = with(ServerRequestHintHelper(this)) {
     hint(key, value)
 }
-
-fun <T : Any> ServerRequest.bodyToMono(elementClass: KClass<T>): Mono<T> = this.bodyToMono(elementClass.java)
-fun <T : Any> ServerRequest.bodyToFlux(elementClass: KClass<T>): Flux<T> = this.bodyToFlux(elementClass.java)
 
 class ServerRequestHintHelper(
     private val serverRequest: ServerRequest
