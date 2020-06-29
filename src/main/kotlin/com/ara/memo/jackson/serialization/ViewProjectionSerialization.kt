5d985eb6d7ea424f937c7808a555ba165db9bcb3
siyual.park <kdpark0723@gmail.com>
2020-06-30 08:48:05 +0900

    재귀적으로 지원

--- src/main/kotlin/com/ara/memo/jackson/serialization/ViewProjectionSerialization.kt
+++ src/main/kotlin/com/ara/memo/jackson/serialization/ViewProjectionSerialization.kt
@@ -1,12 +1,15 @@
 package com.ara.memo.jackson.serialization
 
+import com.ara.memo.util.view.Field
+import com.ara.memo.util.view.Fields
 import com.ara.memo.util.view.ViewProjection
 import com.fasterxml.jackson.core.JsonGenerator
+import com.fasterxml.jackson.databind.JsonNode
 import com.fasterxml.jackson.databind.JsonSerializer
 import com.fasterxml.jackson.databind.ObjectMapper
 import com.fasterxml.jackson.databind.SerializerProvider
+import com.fasterxml.jackson.databind.node.ObjectNode
 import org.springframework.stereotype.Component
-import java.util.concurrent.ConcurrentHashMap
 
 @Component
 class ViewProjectionSerialization(
@@ -14,10 +17,23 @@ class ViewProjectionSerialization(
 ) : JsonSerializer<ViewProjection<*>>() {
     override fun serialize(value: ViewProjection<*>, gen: JsonGenerator, serializers: SerializerProvider) {
         val jsonNode = objectMapper.readTree(objectMapper.writeValueAsString(value.view))
-        val result = objectMapper.updateValue(ConcurrentHashMap<String, Any>(), jsonNode)
+        removeFields(jsonNode, value.fields)
+        gen.writeObject(jsonNode)
+    }
 
-        result.keys.forEach { if (!value.fields.contains(it)) result.remove(it) }
-
-        gen.writeObject(result)
+    private fun removeFields(node: JsonNode, fields: Fields) {
+        if (node is ObjectNode) {
+            val deleteFields = mutableSetOf<String>()
+            val remainFields = mutableSetOf<Field>()
+            node.fieldNames().forEach {
+                val localFields = fields[it]
+                if (localFields == null) deleteFields.add(it)
+                else remainFields.add(localFields)
+            }
+            deleteFields.forEach { node.remove(it) }
+            remainFields.forEach { removeFields(node[it.name], it.children) }
+        } else {
+            node.forEach { removeFields(it, fields) }
+        }
     }
 }
--- src/main/kotlin/com/ara/memo/util/view/ViewProjection.kt
+++ src/main/kotlin/com/ara/memo/util/view/ViewProjection.kt
@@ -4,15 +4,15 @@ import kotlin.reflect.KClass
 
 data class ViewProjection<T : Any>(
     val view: View<T>,
-    val fields: Collection<String>
+    val fields: Fields
 ) {
     companion object {
         fun <T : Any> of(view: View<T>, fields: Collection<String>): ViewProjection<T> {
-            return ViewProjection(view, fields)
+            return ViewProjection(view, Fields.from(fields))
         }
 
         fun <T : Any> of(value: T, fields: Collection<String>, view: KClass<*>? = null): ViewProjection<T> {
-            return ViewProjection(View.of(value, view), fields)
+            return ViewProjection(View.of(value, view), Fields.from(fields))
         }
     }
 }
