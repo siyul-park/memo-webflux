34f5fe344496a92dbda8755ffc46ec93eb35806a
siyual.park <kdpark0723@gmail.com>
2020-06-30 08:56:53 +0900

    리팩토링

--- src/main/kotlin/com/ara/memo/handler/user/AbstractReadUserHandler.kt
+++ src/main/kotlin/com/ara/memo/handler/user/AbstractReadUserHandler.kt
@@ -2,13 +2,14 @@ package com.ara.memo.handler.user
 
 import com.ara.memo.dto.user.UserView
 import com.ara.memo.handler.Handler
+import com.ara.memo.util.view.View
 import com.ara.memo.util.view.ViewProjection
 import org.reactivestreams.Publisher
 import org.springframework.web.reactive.function.server.ServerResponse
 import reactor.core.publisher.Mono
 
 abstract class AbstractReadUserHandler : Handler {
-    protected fun createResponse(userView: Publisher<ViewProjection<UserView>>): Mono<ServerResponse> {
+    protected fun createResponse(userView: Publisher<ViewProjection<View<UserView>>>): Mono<ServerResponse> {
         return ServerResponse.ok()
             .body(userView, ViewProjection::class.java)
     }
--- src/main/kotlin/com/ara/memo/util/view/ViewProjection.kt
+++ src/main/kotlin/com/ara/memo/util/view/ViewProjection.kt
@@ -3,15 +3,15 @@ package com.ara.memo.util.view
 import kotlin.reflect.KClass
 
 data class ViewProjection<T : Any>(
-    val view: View<T>,
+    val view: T,
     val fields: Fields
 ) {
     companion object {
-        fun <T : Any> of(view: View<T>, fields: Collection<String>): ViewProjection<T> {
+        fun <T : Any> of(view: View<T>, fields: Collection<String>): ViewProjection<View<T>> {
             return ViewProjection(view, Fields.from(fields))
         }
 
-        fun <T : Any> of(value: T, fields: Collection<String>, view: KClass<*>? = null): ViewProjection<T> {
+        fun <T : Any> of(value: T, fields: Collection<String>, view: KClass<*>? = null): ViewProjection<View<T>> {
             return ViewProjection(View.of(value, view), Fields.from(fields))
         }
     }
